bf4567975dfaf5df6532ee1f3d51b26f6183c44b
JosieMCantu <josiex2@gmail.com>
2021-05-13 15:15:58 -0700

    passes first test

---
+++ src/containers/RestyContainer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RestyContainer extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hello</h1>
+            </>
+        )
+    }
+}
---
+++ src/containers/RestyContainer.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RestyContainer from './RestyContainer';
+
+describe('', () => {
+    it('', async () => {
+        render(<RestyContainer />)
+
+        screen.getByText('Hello', {exact: false})
+    })
+})
